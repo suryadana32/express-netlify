de21617d430ee6a5f27b9be554990f9c929ad134
Putu Eka Suryadana <esuryadana@gmail.com>
2022-02-23 23:03:46 +0800

    netlify 6

--- src/api.js
+++ src/api.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const serverless = require("serverless-http");
-
+const categoryRoutes = require("../routes/blog");
 const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
+app.use(express.json());
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/api`, categoryRoutes);
 
 module.exports = app;
 module.exports.handler = serverless(app);
